6ee96014c1ee12b511e5861d6fe1ee058ccc7ce6
Mariam Yohannes <84715648+MYGBM@users.noreply.github.com>
2023-09-01 14:25:46 +0300

    Update 0001-two-sum.py
    removed unnecessary comment

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -5,10 +5,6 @@ class Solution:
              if secondNum in nums[i+1:]:
                  j=nums[i+1:].index(secondNum)+i+1
                  return(i,j)
-        #     for j in range(i+1,end):
-        #         if nums[i]+nums[j]==target:
-        #             return(i,j)
-
 
 
         
