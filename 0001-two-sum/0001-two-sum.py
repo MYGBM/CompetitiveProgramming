438635607a2ce83d2c29f4f36367c08e5c464dd4
Mariam Gustavo <yegetamariam@gmail.com>
2023-09-01 14:20:56 +0300

    Improved solution 0(n)

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,10 +1,9 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        end=len(nums)
-        for i in range(0,end) :
-            for j in range(i+1,end):
-                if nums[i]+nums[j]==target:
-                    return(i,j)
-
+         for i,num in enumerate(nums) :
+             secondNum=target-num
+             if secondNum in nums[i+1:]:
+                 j=nums[i+1:].index(secondNum)+i+1
+                 return(i,j)
 
         
