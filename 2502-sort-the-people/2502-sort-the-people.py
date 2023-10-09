2b92c7be35e38041c7f18cbaae31324007535496
Mariam Yohannes <84715648+MYGBM@users.noreply.github.com>
2023-10-10 00:55:40 +0300

    Time: 545 ms (7.13%), Space: 16.8 MB (82.06%) - LeetHub

--- 2502-sort-the-people/2502-sort-the-people.py
+++ 2502-sort-the-people/2502-sort-the-people.py
@@ -1,15 +1,24 @@
 class Solution:
     def sortPeople(self, names: List[str], heights: List[int]) -> List[str]:
-        n=len(names)
-        for i in range(n):
-            max_index=i
-            for j in range(i+1,n):
-                if heights[j]>heights[max_index]:
-                    max_index=j
-            heights[i],heights[max_index]=heights[max_index],heights[i]
-            names[i],names[max_index]=names[max_index],names[i]
-        return names 
-                    
+         
+        heightToName={}
+        for height,name in zip(heights,names):
+            heightToName[height]=name
 
+        n=len(heights)
+        for i in range(1,n):
+            key=heights[i]
+            j=i-1
+            while j>=0 and heights[j]<key:
+                heights[j+1]=heights[j]
+                j-=1
+                heights[j+1]=key
+            
+        #map the sorted height back to the names
+        names_list=[]
+        for height in heights:
+            names_list.append(heightToName[height])
+        return names_list
 
+#problem insertion sort is supposed to be in place 
         
