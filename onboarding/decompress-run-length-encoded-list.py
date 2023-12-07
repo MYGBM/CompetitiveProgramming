76f61b256f08228690240875591a73b23b836b69
Mariam Yohannes <84715648+MYGBM@users.noreply.github.com>
2023-12-07 10:55:19 +0300

    Added solution for Decompress Run-Length Encoded List

--- onboarding/decompress-run-length-encoded-list.py
+++ onboarding/decompress-run-length-encoded-list.py
@@ -1,31 +1,17 @@
 class Solution:
     def decompressRLElist(self, nums: List[int]) -> List[int]:
-        output = []
+        decompressed =[]
         
+        for i in range(len(nums)//2):
+            tempDecompressed=[]
+            freq, val = nums[2*i], [nums[2*i+1]]
+            tempDecompressed = val * freq
+            decompressed +=tempDecompressed
         
-        i = 0
-        while i<len(nums)-1:
-            temp = []
-            if nums[i+1]<=9:
-                temp = str(nums[i+1])*nums[i]
-                temp = list(temp)
-                
-            elif nums[i+1]<=99:
-                temp = str(nums[i+1])*nums[i]
-                temp =[temp[i:i+2] for i in range(0,len(temp),2)]
-                
-            else:
-                temp = str(nums[i+1])*nums[i]
-                temp =[temp[i:i+3] for i in range(0,len(temp),3)]
-                
-            output.extend(temp)
+        return decompressed
             
             
-            i += 2
             
-        output=[int(o) for o in output]
-        
-        return output
         
         
-        
+            
