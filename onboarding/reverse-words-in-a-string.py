5af7135a294b9f41256745c385ea76b13317afa7
Mariam Yohannes <84715648+MYGBM@users.noreply.github.com>
2023-12-13 11:50:57 +0300

    Added solution for Reverse Words in a String

--- onboarding/reverse-words-in-a-string.py
+++ onboarding/reverse-words-in-a-string.py
@@ -1,5 +1,11 @@
 class Solution:
     def reverseWords(self, s: str) -> str:
-        newS = s.split()
-        newS = " ".join(reversed(newS))
-        return newS
+        sentence= s.split()
+        L= 0
+        R= len(sentence)-1
+        while L< R:
+            sentence[L], sentence[R] = sentence[R], sentence[L]
+            L += 1
+            R -= 1
+        return ' '.join(sentence)
+        
